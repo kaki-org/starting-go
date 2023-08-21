141e950912af4ce97cb42b59521bb90d07a1ec52
kakikubo <kakikubo@gmail.com>
2023-08-22 07:26:28 +0900

    sliceのテストを追加

--- zoo/slice-map-channel_test.go
+++ zoo/slice-map-channel_test.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestSlice(t *testing.T) {
 	s := make([]int, 10)
 
+	fmt.Println(s)
 	expect := 10
+	expect_value := 0
 	actual := s
 	if expect != len(actual) {
 		t.Errorf("%d != %d", expect, actual)
+	}
+	if expect_value != actual[9] {
+		t.Errorf("%d != %d", expect_value, actual[0])
 	}
 }
