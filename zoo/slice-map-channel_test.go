150dac02eceb72d08cc895fecbb7300b49036159
kakikubo <kakikubo@gmail.com>
2023-08-22 07:34:38 +0900

    スライスの型を出力してみる。配列と見分けがつかない。

--- zoo/slice-map-channel_test.go
+++ zoo/slice-map-channel_test.go
@@ -6,8 +6,12 @@ import (
 )
 
 func TestSlice(t *testing.T) {
+	var a [10]int
 	s := make([]int, 10)
 
+	fmt.Printf("こっちは配列%T\n", a)
+	fmt.Println(a)
+	fmt.Printf("こっちはスライス%T\n", s)
 	fmt.Println(s)
 	expect := 10
 	expect_value := 0
