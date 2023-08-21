c982bc5458da2c23b0b506a0a61821728bb88533
kakikubo <kakikubo@gmail.com>
2023-08-22 08:04:57 +0900

    スライスのキャパシティ

--- zoo/slice-map-channel_test.go
+++ zoo/slice-map-channel_test.go
@@ -25,3 +25,23 @@ func TestSlice(t *testing.T) {
 	// 要素数を超えた値を指定するとpanicになる
 	// fmt.Println(actual[10]) // panic: runtime error: index out of range
 }
+
+func TestSliceCap(t *testing.T) {
+	/* 要素数5、容量5のスライス */
+	s5 := make([]int, 5)
+	fmt.Printf("len=%d cap=%d value=%v\n", len(s5), cap(s5), s5)
+	/* 要素数5、容量10のスライス */
+	s10 := make([]int, 5, 10)
+	fmt.Printf("len=%d cap=%d value=%v\n", len(s10), cap(s10), s10)
+	expect_cap5 := 5
+	expect_cap10 := 10
+	if (expect_cap5 != cap(s5)) || (expect_cap10 != cap(s10)) {
+		t.Errorf("%d != %d", expect_cap5, cap(s5))
+		t.Errorf("%d != %d", expect_cap10, cap(s10))
+	}
+	// fmt.Println(s10[9]) // これはpanicになる
+	s10[4] = 100
+	fmt.Println(s10[4])
+	fmt.Printf("%#v\n", s10[0:10]) // 0から9までの要素を取得
+
+}
