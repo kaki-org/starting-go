508f7c82da89deb74b0977cb0ef0f85698f7dcf7
kakikubo <kakikubo@gmail.com>
2023-08-23 04:51:48 +0900

    簡易スライス式

--- zoo/slice-map-channel_test.go
+++ zoo/slice-map-channel_test.go
@@ -58,3 +58,35 @@ func TestSliceCap(t *testing.T) {
 		t.Errorf("%v != %v", s5, s)
 	}
 }
+
+func TestSimpleSliceExpressions(t *testing.T) {
+	a := [5]int{1, 2, 3, 4, 5}
+	actual1 := a[0:2]
+	actual2 := a[2:]
+	actual3 := a[:4]
+	actual4 := a[:]
+	actual5 := a[len(a) -2:] // 式もかける
+
+	expect1 := []int{1, 2}
+	expect2 := []int{3, 4, 5}
+	expect3 := []int{1, 2, 3, 4}
+	expect4 := []int{1, 2, 3, 4, 5}
+	expect5 := []int{4,5}
+
+	if !reflect.DeepEqual(expect1, actual1) {
+		t.Errorf("%v != %v", expect1, actual1)
+	}
+	if !reflect.DeepEqual(expect2, actual2) {
+		t.Errorf("%v != %v", expect2, actual2)
+	}
+	if !reflect.DeepEqual(expect3, actual3) {
+		t.Errorf("%v != %v", expect3, actual3)
+	}
+	if !reflect.DeepEqual(expect4, actual4) {
+		t.Errorf("%v != %v", expect4, actual4)
+	}
+	if !reflect.DeepEqual(expect5, actual5) {
+		t.Errorf("%v != %v", expect5, actual5)
+	}
+
+}
