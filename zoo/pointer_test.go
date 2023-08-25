1c0c39a9bc33bb2cf402bcae8792267b571b0046
kakikubo <kakikubo@gmail.com>
2023-08-26 07:22:45 +0900

    &(アドレス演算子)

--- zoo/pointer_test.go
+++ zoo/pointer_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -11,4 +12,36 @@ func TestPointerInitialize(t *testing.T) {
 	if p != nil {
 		t.Errorf("ポインタ変数はnilではありません。")
 	}
+}
+
+func TestAddressOperator(t *testing.T) {
+	// &演算子で変数のアドレスを取得
+	var i int
+	p := &i // pは*iのポインタ型
+
+	fmt.Printf("%T\n", p) // *int
+	if p == nil {
+		t.Errorf("ポインタ変数はnilです。")
+	}
+
+	pp := &p               // ppは**intのポインタ型
+	fmt.Printf("%T\n", pp) // **int
+	if pp == nil {
+		t.Errorf("ポインタ変数はnilです。")
+	}
+
+	i = 5
+	expect := 5
+	actual := *p // *pはiのエイリアス
+	if expect != actual {
+		t.Errorf("%d != %d", expect, actual)
+	}
+
+	*p = 10
+	expect = 10
+	actual = i
+	if expect != actual {
+		t.Errorf("%d != %d", expect, actual)
+	}
+
 }
