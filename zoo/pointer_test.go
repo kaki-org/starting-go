68ce27c2a4545d0c9f196e1b8c263e449cb00a7e
kakikubo <kakikubo@gmail.com>
2023-08-26 07:42:25 +0900

    int型配列のポインタを渡し、デリファレンスして中身を書き換える例

--- zoo/pointer_test.go
+++ zoo/pointer_test.go
@@ -61,3 +61,19 @@ func TestPointerArgument(t *testing.T) {
 		t.Errorf("%d != %d", expect, actual)
 	}
 }
+
+func pow(p *[3]int) {
+	i := 0
+	for i < 3 {
+		/* 各要素を累乗する */
+		(*p)[i] = (*p)[i] * (*p)[i]
+		i++
+	}
+}
+
+func TestPointerArray(t *testing.T) {
+	/* 変数pは*[3]int型 */
+	p := &[3]int{1, 2, 3}
+	pow(p)
+	fmt.Println(p) // [1 4 9]
+}
