58f0c95322285eef2843dc122cbf41729e79cb08
kakikubo <kakikubo@gmail.com>
2023-08-30 07:34:35 +0900

    hello.goをgetargsとしてビルドする例を追記

--- hello.go
+++ hello.go
@@ -8,6 +8,16 @@ import (
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	// go build -o getargs hello.go
+	/* os.Argsの要素数を表示 */
+	fmt.Printf("length=%d\n", len(os.Args))
+
+	/* os.Argsの内容を表示 */
+	for i, v := range os.Args {
+		fmt.Printf("args[%d]=%s\n", i, v)
+	}
+
 	_, err := os.Open("no-such-file")
 	if err != nil {
 		log.Fatal(err) // os.Exit(1)も呼び出される
