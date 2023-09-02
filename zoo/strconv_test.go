3ba71a9e738ce3b25ce828d88cd682ec0f410390
kakikubo <kakikubo@gmail.com>
2023-09-03 07:27:57 +0900

    浮動小数点数を文字列に変換する

--- zoo/strconv_test.go
+++ zoo/strconv_test.go
@@ -42,3 +42,55 @@ func TestFormatInt(t *testing.T) {
 		t.Errorf("expect %s, but got %s", expectItoa, itoaStr)
 	}
 }
+
+func TestFormatFloat(t *testing.T) {
+	// 指数表現による文字列化
+	expect := "1.23456E+02"
+	floatStr := strconv.FormatFloat(123.456, 'E', -1, 64)
+	if floatStr != expect {
+		t.Errorf("expect %s, but got %s", expect, floatStr)
+	}
+
+	// 指数表現による文字列化(小数点以下2桁まで)
+	expect = "1.23e+02"
+	floatStr = strconv.FormatFloat(123.456, 'e', 2, 64)
+	if floatStr != expect {
+		t.Errorf("expect %s, but got %s", expect, floatStr)
+	}
+
+	// 実数表現による文字列化
+	expect = "123.456"
+	floatStr = strconv.FormatFloat(123.456, 'f', -1, 64)
+	if floatStr != expect {
+		t.Errorf("expect %s, but got %s", expect, floatStr)
+	}
+
+	// 実数表現による文字列化(小数点2桁まで)
+	expect = "123.46"
+	floatStr = strconv.FormatFloat(123.456, 'f', 2, 64)
+	if floatStr != expect {
+		t.Errorf("expect %s, but got %s", expect, floatStr)
+	}
+
+	// 指数部の大きさで変動する表現による文字列化
+	expect = "123.456"
+	floatStr = strconv.FormatFloat(123.456, 'g', -1, 64)
+	if floatStr != expect {
+		t.Errorf("expect %s, but got %s", expect, floatStr)
+	}
+
+	// 指数部の大きさで変動する表現による文字列化(仮数部全体が4桁まで)
+	expect = "123.5"
+	floatStr = strconv.FormatFloat(123.456, 'g', 4, 64)
+	if floatStr != expect {
+		t.Errorf("expect %s, but got %s", expect, floatStr)
+	}
+
+	// 指数部の大きさで変動する表現による文字列化(仮数部全体が8桁まで)
+	expect = "1.2345679E+08"
+	floatStr = strconv.FormatFloat(123456789.123, 'G', 8, 64)
+	if floatStr != expect {
+		t.Errorf("expect %s, but got %s", expect, floatStr)
+	}
+
+}
