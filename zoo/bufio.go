88f883759dd538503a538e487fb74b67a5291038
kakikubo <kakikubo@gmail.com>
2023-09-04 06:37:21 +0900

    bufioの利用例

--- zoo/bufio.go
+++ zoo/bufio.go
@@ -4,15 +4,28 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
+	s := `このプログラムはstrings.NewReader()関数を使って与えられた文字列を
+io.Readerインターフェースと互換性のあるstrings.Reader型を生成します
+なにか文字列を入力してEnterキーをおしてみてください。終了はCtrl+Dです`
+
+	/* 文字列からstrings.Readerを生成 */
+	r := strings.NewReader(s)
+
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
 	/* 標準入力をソースにしたスキャナの生成 */
-	scanner := bufio.NewScanner(os.Stdin)
+	scanner = bufio.NewScanner(os.Stdin)
 
 	/* 入力のスキャンが成功する限り繰り返すループ */
 	for scanner.Scan() {
-		fmt.Println(scanner.Text()) // Ctrl+Dで終了
+		fmt.Println("=> ", scanner.Text()) // Ctrl+Dで終了
 	}
 
 	/* スキャン中にエラーが発生した場合の処理 */
