f50165efa9250ea0b6aed6297f95bcfd5d9c8507
kakikubo <kakikubo@gmail.com>
2023-09-05 06:26:56 +0900

    正規表現をコンパイルする

--- zoo/regex_test.go
+++ zoo/regex_test.go
@@ -5,9 +5,16 @@ import (
 	"testing"
 )
 
-func TestRegexMatchString(t *testing.T) {
+func TestRegexpMatchString(t *testing.T) {
 	m, _ := regexp.MatchString("A", "ABC")
 	expect(t, m, true)
 	m, _ = regexp.MatchString("A", "XYZ")
 	expect(t, m, false)
+}
+
+func TestRegexpCompile(t *testing.T) {
+	// 正規表現のパターンをコンパイルする
+	r, _ := regexp.Compile("A")
+	expect(t, r.MatchString("ABC"), true)
+	expect(t, r.MatchString("XYZ"), false)
 }
