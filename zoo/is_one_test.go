f019afd95ee75c458a29cfb29c7bf0cdb42c7e0f
kakikubo <kakikubo@gmail.com>
2023-09-07 07:49:20 +0900

    isOneのfalseケースも追加

--- zoo/is_one_test.go
+++ zoo/is_one_test.go
@@ -11,3 +11,11 @@ func TestIsOne(t *testing.T) {
 		t.Errorf("%d is not one.", n)
 	}
 }
+
+func TestIsOneFalse(t *testing.T) {
+	n := 100
+	b := IsOne(n)
+	if b == true {
+		t.Errorf("%d is not one.", n)
+	}
+}
