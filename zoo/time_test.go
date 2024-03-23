5c10ba538360cbdc6d50281bd399b2d54c316779
kakikubo <kakikubo@gmail.com>
2024-03-23 18:09:16 +0900

    string用format

--- zoo/time_test.go
+++ zoo/time_test.go
@@ -6,13 +6,14 @@ import (
 	"time"
 )
 const fixedTimeString = "2006-01-02 15:04:05"
+const stringExpectFormat = "%s != %s"
 
 func TestTimeStandard(t *testing.T) {
 	tm := time.Date(2023, 8, 31, 7, 15, 30, 0, time.Local)
 	expect := "2023-08-31 07:15:30"
 	actual := tm.Format(fixedTimeString)
 	if expect != actual {
-		t.Errorf("%s != %s", expect, actual)
+		t.Errorf(stringExpectFormat, expect, actual)
 	}
 	fmt.Println(tm)
 	fmt.Println(tm.Year())
@@ -51,7 +52,7 @@ func TestTimeDuration(t *testing.T) {
 
 	// 日本時間2023/09/01 7:00:00を表すTimeが生成された事を確認
 	if expect != actual {
-		t.Errorf("%s != %s", expect, actual)
+		t.Errorf(stringExpectFormat, expect, actual)
 	}
 }
 
@@ -63,7 +64,7 @@ func TestTimeDurationSub(t *testing.T) {
 	expect := "44400h0m0s"
 	actual := d.String()
 	if expect != actual {
-		t.Errorf("%s != %s", expect, actual)
+		t.Errorf(stringExpectFormat, expect, actual)
 	}
 }
 
@@ -106,7 +107,7 @@ func TestTimeAddDate(t *testing.T) {
 	expect := "2024-11-04 07:00:00"
 	actual := tm2.Format("2006-01-02 15:04:05")
 	if expect != actual {
-		t.Errorf("%s != %s", expect, actual)
+		t.Errorf(stringExpectFormat, expect, actual)
 	}
 
 	tm3 := tm2.AddDate(-1, -2, -3)
@@ -123,14 +124,14 @@ func TestTimeFormat(t *testing.T) {
 	expect := "2015-10-01 00:00:00"
 	actual := tm.Format(fixedTimeString)
 	if expect != actual {
-		t.Errorf("%s != %s", expect, actual)
+		t.Errorf(stringExpectFormat, expect, actual)
 	}
 
 	// tm.Format で生成したtimeはUTCで生成される
 	expect = "UTC"
 	actual, _ = tm.Zone()
 	if expect != actual {
-		t.Errorf("%s != %s", expect, actual)
+		t.Errorf(stringExpectFormat, expect, actual)
 	}
 
 	// time.ParseでRFC822形式の文字列をパースする
@@ -142,7 +143,7 @@ func TestTimeFormat(t *testing.T) {
 	expect = "2015-10-01 00:00:00"
 	actual = tm2.Format(fixedTimeString) // 時刻形式に%Yなどは使わない
 	if expect != actual {
-		t.Errorf("%s != %s", expect, actual)
+		t.Errorf(stringExpectFormat, expect, actual)
 	}
 
 	// 日本語を混ぜた文字列もパース可能
@@ -150,7 +151,7 @@ func TestTimeFormat(t *testing.T) {
 	expect = "2015-10-01 00:00:00"
 	actual = jt.Format(fixedTimeString)
 	if expect != actual {
-		t.Errorf("%s != %s", expect, actual)
+		t.Errorf(stringExpectFormat, expect, actual)
 	}
 
 	// Parseに足りない要素(時,分,秒)は初期値が入る
@@ -184,25 +185,25 @@ func TestGenerateStringFromTime(t *testing.T) {
 	expectRFC822 := "01 Sep 23 07:00 JST"
 	actualRFC822 := tm.Format(time.RFC822)
 	if expectRFC822 != actualRFC822 {
-		t.Errorf("%s != %s", expectRFC822, actualRFC822)
+		t.Errorf(stringExpectFormat, expectRFC822, actualRFC822)
 	}
 
 	expectRFC3339Nano := "2023-09-01T07:00:00+09:00"
 	actualRFC3339Nano := tm.Format(time.RFC3339Nano)
 	if expectRFC3339Nano != actualRFC3339Nano {
-		t.Errorf("%s != %s", expectRFC3339Nano, actualRFC3339Nano)
+		t.Errorf(stringExpectFormat, expectRFC3339Nano, actualRFC3339Nano)
 	}
 
 	expectTokyo := "2023年9月1日 07時00分00秒"
 	actualTokyo := tm.Format("2006年1月2日 15時04分05秒")
 	if expectTokyo != actualTokyo {
-		t.Errorf("%s != %s", expectTokyo, actualTokyo)
+		t.Errorf(stringExpectFormat, expectTokyo, actualTokyo)
 	}
 
 	expect := "2023/09/01"
 	actual := tm.Format("2006/01/02")
 	if expect != actual {
-		t.Errorf("%s != %s", expect, actual)
+		t.Errorf(stringExpectFormat, expect, actual)
 	}
 }
 
@@ -216,7 +217,7 @@ func TestTimeUTC(t *testing.T) {
 	expect := "2023-09-01 00:00:00 +0000 UTC"
 	actual := tmUTC.String()
 	if expect != actual {
-		t.Errorf("%s != %s", expect, actual)
+		t.Errorf(stringExpectFormat, expect, actual)
 	}
 }
 
@@ -230,7 +231,7 @@ func TestTimeJST(t *testing.T) {
 	expect := "2023-09-01 09:00:00 +0900 JST"
 	actual := tmJST.String()
 	if expect != actual {
-		t.Errorf("%s != %s", expect, actual)
+		t.Errorf(stringExpectFormat, expect, actual)
 	}
 }
 
@@ -249,7 +250,7 @@ func TestTimeUnix(t *testing.T) {
 	expect2 := "2015-11-27 15:00:00 +0000 UTC"
 	actual2 := tm2.UTC().String()
 	if expect2 != actual2 {
-		t.Errorf("%s != %s", expect2, actual2)
+		t.Errorf(stringExpectFormat, expect2, actual2)
 	}
 }
 
